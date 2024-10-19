5563256362cf6351cd5fb792b1366b8ab056d221
TrNixo <txnixo0126@gmail.com>
2024-10-19 23:42:58 +0700

    add more logs

--- cluster/http_agent.go
+++ cluster/http_agent.go
@@ -148,8 +148,12 @@ func (h *httpAgent) ResponseMid(mid uint64, v interface{}) error {
 	}
 	if h.responseChan != nil {
 		log.Infof("[HTTP Agent] response chan found set to httpCtx: %s", data)
-		h.responseChan <- data
-		log.Infof("[HTTP Agent] response chan sent: %s", data)
+		select {
+		case h.responseChan <- data:
+			log.Infof("[HTTP Agent] response chan sent: %s", data)
+		default:
+			log.Infof("[HTTP Agent] response chan is blocked, unable to send: %s", data)
+		}
 	} else {
 		log.Infof("[HTTP Agent] response chan not found set to httpCtx: %s", data)
 		h.httpCtx.SetBody(data)
